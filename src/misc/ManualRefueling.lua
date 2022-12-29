49003ee0c6d1eb49ac3b280b8d88ee3f0e4b317c
4c65736975 <107006334+4c65736975@users.noreply.github.com>
2022-12-29 20:43:45 +0100

    Upload latest version

--- src/misc/ManualRefueling.lua
+++ src/misc/ManualRefueling.lua
@@ -1,34 +1,38 @@
 -- Author: VertexFloat
--- Date: 06.06.2022
--- Version: Farming Simulator 22, 1.0.0.0
+-- Date: 05.07.2022
+-- Version: Farming Simulator 22, 1.0.0.1
 -- Copyright (C): VertexFloat, All Rights Reserved
 -- Manual Refueling
 
+-- Changelog (1.0.0.1) :
+--
+-- improved and more clearly code
+-- fixed refilling from some placeables
+
 ManualRefueling = {
-    OLD_TRIGGER_COLMASK = 1075838976,
-    OLD_TRIGGER_COLMASK2 = 2097152,
-    NEW_TRIGGER_COLMASK = 1076887552,
-    NEW_TRIGGER_COLMASK2 = 3145728
+    OBJECT_MASK = {
+        OLD = {
+            1075838976,
+            2097152
+        },
+        NEW = {
+            1076887552,
+            3145728
+        }
+    }
 }
 
-local function onFinishedLoading()
-    ManualRefueling:updateLoadTriggersColMask()
-end
-
-FSBaseMission.onFinishedLoading = Utils.appendedFunction(FSBaseMission.onFinishedLoading, onFinishedLoading)
-
 local function onFinalizePlacement()
-    ManualRefueling:updateLoadTriggersColMask()
+    ManualRefueling:updateLoadTriggersColMask(ManualRefueling.OBJECT_MASK)
 end
 
 Placeable.finalizePlacement = Utils.appendedFunction(Placeable.finalizePlacement, onFinalizePlacement)
 
 function ManualRefueling:loadTriggerCallback(superFunc, triggerId, otherId, onEnter, onLeave, onStay, otherShapeId)
-    local isManual = ManualRefueling:getIsLoadTriggerManual(triggerId)
     local fillableObject = g_currentMission:getNodeObject(otherId)
 
+    self.isManual = ManualRefueling:getIsLoadTriggerManual(triggerId)
     self.playerInTrigger = false
-    self.currentTrigger = triggerId
 
     if fillableObject ~= nil and fillableObject ~= self.source and fillableObject.getRootVehicle ~= nil and fillableObject.getFillUnitIndexFromNode ~= nil then
         local fillTypes = self.source:getSupportedFillTypes()
@@ -95,7 +99,7 @@ function ManualRefueling:loadTriggerCallback(superFunc, triggerId, otherId, onEn
                     if not self.isLoading and next(self.fillableObjects) ~= nil and self:getIsFillableObjectAvailable() then
                         self:toggleLoading()
                     end
-                elseif next(self.fillableObjects) ~= nil and not isManual then
+                elseif next(self.fillableObjects) ~= nil and not self.isManual then
                     g_currentMission.activatableObjectsSystem:addActivatable(self.activatable)
                 else
                     g_currentMission.activatableObjectsSystem:removeActivatable(self.activatable)
@@ -104,7 +108,7 @@ function ManualRefueling:loadTriggerCallback(superFunc, triggerId, otherId, onEn
         end
     end
 
-    if isManual then
+    if self.isManual then
         if g_currentMission.player ~= nil and otherId == g_currentMission.player.rootNode then
             if onEnter and next(self.fillableObjects) ~= nil and g_currentMission.player.isControlled then
                 g_currentMission.activatableObjectsSystem:addActivatable(self.activatable)
@@ -112,6 +116,12 @@ function ManualRefueling:loadTriggerCallback(superFunc, triggerId, otherId, onEn
                 self.playerInTrigger = true
             else
                 g_currentMission.activatableObjectsSystem:removeActivatable(self.activatable)
+
+                if self.isLoading then
+                    if not self.isElectric then
+                        self:setIsLoading(false)
+                    end
+                end
 
                 self.playerInTrigger = false
             end
@@ -124,30 +134,53 @@ LoadTrigger.loadTriggerCallback = Utils.overwrittenFunction(LoadTrigger.loadTrig
 function ManualRefueling:getIsFillableObjectAvailable()
     if next(self.fillableObjects) == nil then
         return false
-    elseif self.isLoading then
-        if self.currentFillableObject ~= nil and self:getAllowsActivation(self.currentFillableObject) then
-            return true
-        end
     else
-        self.validFillableObject = nil
-        self.validFillableFillUnitIndex = nil
-        local numOfObjects = 0
+        if self.isLoading then
+            if self.currentFillableObject ~= nil and self:getAllowsActivation(self.currentFillableObject) then
+                return true
+            end
+        else
+            self.validFillableObject = nil
+            self.validFillableFillUnitIndex = nil
 
-        for _, fillableObject in pairs(self.fillableObjects) do
-            numOfObjects = numOfObjects + 1
-        end
+            -- last object that was filled has lower prio than the other objects in the trigger
+            -- so we can guarantee that all objects will be filled
+            -- !
+            -- it's counting all of components so for one vehicle if it has two or more components it's blocking action when we stop refilling and
+            -- we must enter trigger with vehicle again, so, so far for manual refilling trigger it's disabled and always return one vehicle in trigger
+            -- !
+            local hasLowPrioObject = false
+            local numOfObjects = 0
 
-        for _, fillableObject in pairs(self.fillableObjects) do
-            if self:getAllowsActivation(fillableObject.object) and fillableObject.object:getFillUnitSupportsToolType(fillableObject.fillUnitIndex, ToolType.TRIGGER) then
-                if not self.source:getIsFillAllowedToFarm(self:farmIdForFillableObject(fillableObject.object)) then
-
-                    return false
+            for _, fillableObject in pairs(self.fillableObjects) do
+                if fillableObject.lastWasFilled then
+                    hasLowPrioObject = true
                 end
 
-                self.validFillableObject = fillableObject.object
-                self.validFillableFillUnitIndex = fillableObject.fillUnitIndex
+                if self.isManual then
+                    numOfObjects = 1
+                else
+                    numOfObjects = numOfObjects + 1
+                end
+            end
 
-                return true
+            hasLowPrioObject = hasLowPrioObject and (numOfObjects > 1)
+
+            for _, fillableObject in pairs(self.fillableObjects) do
+                if not fillableObject.lastWasFilled or not hasLowPrioObject then
+                    if self:getAllowsActivation(fillableObject.object) then
+                        if fillableObject.object:getFillUnitSupportsToolType(fillableObject.fillUnitIndex, ToolType.TRIGGER) then
+                            if not self.source:getIsFillAllowedToFarm(self:farmIdForFillableObject(fillableObject.object)) then
+                                return false
+                            end
+
+                            self.validFillableObject = fillableObject.object
+                            self.validFillableFillUnitIndex = fillableObject.fillUnitIndex
+
+                            return true
+                        end
+                    end
+                end
             end
         end
     end
@@ -158,48 +191,10 @@ end
 LoadTrigger.getIsFillableObjectAvailable = Utils.overwrittenFunction(LoadTrigger.getIsFillableObjectAvailable, ManualRefueling.getIsFillableObjectAvailable)
 
 function ManualRefueling:getAllowsActivation(superFunc, fillableObject)
-    local fillUnitIndex = 1
-    local isEmpty = true
+    local fillUnitIndex = ManualRefueling:getValidFillableFillUnitIndex(fillableObject, self.fillableObjects)
+    local isEmpty = ManualRefueling:getIsSourceNotEmpty(self.source, fillUnitIndex, fillableObject)
 
-    for _, validFillableObject in pairs(self.fillableObjects) do
-        if fillableObject ~= nil then
-            if fillableObject == validFillableObject.object then
-                fillUnitIndex = validFillableObject.fillUnitIndex
-            end
-        end
-    end
-
-    for _, loadTrigger in pairs(self.source.loadTriggers) do
-        if loadTrigger.hasInfiniteCapacity then
-            isEmpty = false
-        end
-    end
-
-    if fillableObject ~= nil then
-        local supportedFillTypes = fillableObject:getFillUnitSupportedFillTypes(fillUnitIndex)
-
-        for _, loadTrigger in pairs(self.source.loadTriggers) do
-            for fillTypeIndex, _ in pairs(loadTrigger.fillTypes) do
-                for supportedFillType, _ in pairs(supportedFillTypes) do
-                    if fillTypeIndex == supportedFillType then
-                        for _, source in pairs(self.source.sourceStorages) do
-                            for fillType, fillLevel in pairs(source.fillLevels) do
-                                if fillTypeIndex == fillType then
-                                    if fillLevel > 0 then
-                                        isEmpty = false
-                                    end
-                                end
-                            end
-                        end
-                    end
-                end
-            end
-        end
-    end
-
-    if not g_currentMission.player.isControlled and self.playerInTrigger then
-        self:setIsLoading(false)
-    end
+    self.isElectric = ManualRefueling:getIsFillableFillTypeIsElectric(self.source, fillUnitIndex, fillableObject)
 
     if not self.requiresActiveVehicle then
         return true
@@ -215,9 +210,7 @@ function ManualRefueling:getAllowsActivation(superFunc, fillableObject)
         end
     end
 
-    local isManual = ManualRefueling:getIsLoadTriggerManual(self.currentTrigger)
-
-    if isManual then
+    if self.isManual then
         if self.playerInTrigger then
             local player = g_currentMission.player.rootNode
             local distance = calcDistanceFrom(fillableObject.rootNode, player)
@@ -232,8 +225,14 @@ function ManualRefueling:getAllowsActivation(superFunc, fillableObject)
                         return true
                     end
                 end
-            else
-                 self:setIsLoading(false)
+            elseif not self.isElectric then
+                self:setIsLoading(false)
+            end
+        end
+
+        if not g_currentMission.player.isControlled then
+            if not self.isElectric then
+                self:setIsLoading(false)
             end
         end
     end
@@ -243,21 +242,28 @@ end
 
 LoadTrigger.getAllowsActivation = Utils.overwrittenFunction(LoadTrigger.getAllowsActivation, ManualRefueling.getAllowsActivation)
 
-function ManualRefueling:updateLoadTriggersColMask()
+function ManualRefueling:updateLoadTriggersColMask(objectsMasks)
     for _, station in pairs(g_currentMission.storageSystem:getLoadingStations()) do
         for _, loadTrigger in pairs(station.loadTriggers) do
-            for i, _ in pairs(loadTrigger.fillTypes) do
-                if (i == g_fillTypeManager:getFillTypeIndexByName("DIESEL")) or (i == g_fillTypeManager:getFillTypeIndexByName("ELECTRICCHARGE")) or (i == g_fillTypeManager:getFillTypeIndexByName("METHANE")) then
+            for index, _ in pairs(loadTrigger.fillTypes) do
+                if (index == g_fillTypeManager:getFillTypeIndexByName("DIESEL")) or (index == g_fillTypeManager:getFillTypeIndexByName("ELECTRICCHARGE")) or (index == g_fillTypeManager:getFillTypeIndexByName("METHANE")) then
                     local collisionMask = getCollisionMask(loadTrigger.triggerNode)
+                    local oldMasks = objectsMasks.OLD
 
-                    if collisionMask == ManualRefueling.OLD_TRIGGER_COLMASK then
-                        setCollisionMask(loadTrigger.triggerNode, ManualRefueling.NEW_TRIGGER_COLMASK)
+                    for i = 1, #oldMasks do
+                        local oldMask = oldMasks[i]
 
-                        loadTrigger.isManual = true
-                    elseif collisionMask == ManualRefueling.OLD_TRIGGER_COLMASK2 then
-                        setCollisionMask(loadTrigger.triggerNode, ManualRefueling.NEW_TRIGGER_COLMASK2)
+                        if collisionMask == oldMask then
+                            local newMasks = objectsMasks.NEW
 
-                        loadTrigger.isManual = true
+                            for j = 1, #newMasks do
+                                local newMask = newMasks[i]
+
+                                setCollisionMask(loadTrigger.triggerNode, newMask)
+
+                                loadTrigger.isManual = true
+                            end
+                        end
                     end
                 else
                     loadTrigger.isManual = false
@@ -273,6 +279,68 @@ function ManualRefueling:getIsLoadTriggerManual(triggerId)
             if loadTrigger.triggerNode == triggerId then
                 if loadTrigger.isManual == true then
                     return true
+                end
+            end
+        end
+    end
+
+    return false
+end
+
+function ManualRefueling:getValidFillableFillUnitIndex(fillableObject, fillableObjects)
+    for _, validFillableObject in pairs(fillableObjects) do
+        if fillableObject ~= nil then
+            if fillableObject == validFillableObject.object then
+                return validFillableObject.fillUnitIndex
+            end
+        end
+    end
+end
+
+function ManualRefueling:getIsSourceNotEmpty(source, fillUnitIndex, fillableObject)
+    if fillableObject ~= nil then
+        local supportedFillTypes = fillableObject:getFillUnitSupportedFillTypes(fillUnitIndex)
+
+        for _, loadTrigger in pairs(source.loadTriggers) do
+            for fillTypeIndex, _ in pairs(loadTrigger.fillTypes) do
+                for supportedFillType, _ in pairs(supportedFillTypes) do
+                    if fillTypeIndex == supportedFillType then
+                        local fillLevels = source:getAllFillLevels(g_currentMission:getFarmId())
+
+                        for fillType, fillLevel in pairs(fillLevels) do
+                            if fillTypeIndex == fillType then
+                                if fillLevel > 0 then
+                                    return false
+                                end
+                            end
+                        end
+                    end
+                end
+            end
+
+            if loadTrigger.hasInfiniteCapacity then
+                return false
+            end
+        end
+    end
+
+    return true
+end
+
+function ManualRefueling:getIsFillableFillTypeIsElectric(source, fillUnitIndex, fillableObject)
+    if fillableObject ~= nil then
+        local supportedFillTypes = fillableObject:getFillUnitSupportedFillTypes(fillUnitIndex)
+
+        for _, loadTrigger in pairs(source.loadTriggers) do
+            for fillTypeIndex, _ in pairs(loadTrigger.fillTypes) do
+                for supportedFillType, _ in pairs(supportedFillTypes) do
+                    if fillTypeIndex == supportedFillType then
+                        local fillTypeName = g_fillTypeManager:getFillTypeNameByIndex(fillTypeIndex)
+
+                        if fillTypeName == "ELECTRICCHARGE" then
+                            return true
+                        end
+                    end
                 end
             end
         end
